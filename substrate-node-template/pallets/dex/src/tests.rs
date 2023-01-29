1f448c725696212ef873158462970abff9cd6aca
Agus <agusrodriguez2456@gmail.com>
2023-01-29 20:15:26 -0300

    update tests

--- substrate-node-template/pallets/dex/src/tests.rs
+++ substrate-node-template/pallets/dex/src/tests.rs
@@ -7,37 +7,6 @@ use frame_support::{
 	},
 };
 
-/* #[test]
-fn it_works_for_default_value() {
-	new_test_ext().execute_with(|| {
-		// Go past genesis block so events get deposited
-		System::set_block_number(1);
-		// Dispatch a signed extrinsic.
-		assert_ok!(Dex::do_something(RuntimeOrigin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(Dex::something(), Some(42));
-		// Assert that the correct event was deposited
-		System::assert_last_event(Event::SomethingStored { something: 42, who: 1 }.into());
-	});
-} */
-
-/* #[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(Dex::cause_error(RuntimeOrigin::signed(1)), Error::<Test>::NoneValue);
-	});
-} */
-
-/* #[test]
-fn test_create_asset() {
-	new_test_ext().execute_with(|| {
-		assert_ok!(Dex::create_asset(RuntimeOrigin::signed(1), 1));
-		// assert asset exists
-		assert!(<<Test as crate::Config>::Fungibles as fungibles::Inspect<_>>::asset_exists(1));
-	});
-} */
-
 #[test]
 fn create_pool_successfully() {
 	new_test_ext().execute_with(|| {
